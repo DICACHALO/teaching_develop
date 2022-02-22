86e8aa2c14a09669af897fc16baebc6be0f60973
dicachalo <dchacon871@unab.edu.co>
2022-02-22 09:23:25 -0500

    First steps JS

---
+++ Class_1/index.js
@@ -0,0 +1,12 @@
+alert("Tercer mensaje de prueba");
+
+document.write("<br><h1>Texto escrito desde el archivo index.js</h1>");
+
+console.log("Mensaje enviado a la consola desde mi archivo Javascript");
+
+//Comentario de una sola línea
+
+/*
+Comentario en bloque
+Varias líneas
+*/
