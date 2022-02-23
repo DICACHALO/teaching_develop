d4b0d39963b392aa6be90885d76b3e43499568fa
dicachalo <dchacon871@unab.edu.co>
2022-02-23 08:02:27 -0500

    Variables

---
+++ Class_2/index.js
@@ -0,0 +1,24 @@
+let nameUser; // Declaramos la variable
+nameUser = "Bob"; // Iniciamos un valor
+
+// let nameUser = "Bob"; Forma resumida
+
+let lastName = "Esponja"; // Tipo de dato es String y se escribe entre comillas
+let score = 35; // Tipo de dato numérico -integer
+let activeUser = true; // Tipo de dato boolean: verdadero(true) o falso(false)
+let age; // Variable que no tiene un valor definido, es diferente a una variable que no existe.
+
+console.log(lastName);
+console.log(nameUser);
+console.log(score);
+console.log(activeUser);
+console.log(age); //undefined
+
+score = 80;
+console.log(score); 
+
+//Variables asociadas a ubicar a una persona
+
+let address = "Calle 29 57-8"; // Tipo de dato String
+let phoneNumber = 30078965; // Tipo de dato numérico
+let colombian = false; // Tipo de dato boolean
