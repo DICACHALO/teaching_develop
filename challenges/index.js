22021346f6aedf68ba905297f329aa3884cb0e3b
dicachalo <dchacon871@unab.edu.co>
2022-03-05 18:43:16 -0500

    challenge 3

--- challenges/index.js
+++ challenges/index.js
@@ -1,5 +1,24 @@
 function greeting(){
-    alert("Hello");
+    nameUser = prompt("Por favor, escribe tu nombre");
+    alert("Hola " + nameUser);
 }
+
+function greeting2(){
+    firstName = prompt("Por favor, escribe tu nombre");
+    lasttName = prompt("Por favor, escribe tu apellido");
+    alert("Hola " + firstName + " " + lasttName);
+}
+
+function sumNumbers(){
+    alert("¡Vamos a sumar!")
+    number1 = prompt("Por favor, escribe un número. Puedes usar decimales.");
+    number1 = parseFloat(number1);
+    number2 = prompt("Escribe un segundo número.");
+    number2 = parseFloat(number2);
+    result = number1 + number2;
+    result = result.toFixed(1);
+    alert("El resultado de sumar " + number1 + " con " + number2 + " es: " + result + ".");
+}
+
 
 
