b7c49e5a72653904ba5a87febd4b8a14fff88ac0
dicachalo <dchacon871@unab.edu.co>
2022-03-05 18:54:25 -0500

    challenge 4

--- challenges/index.js
+++ challenges/index.js
@@ -20,5 +20,19 @@ function sumNumbers(){
     alert("El resultado de sumar " + number1 + " con " + number2 + " es: " + result + ".");
 }
 
+function mathOperation(){
+    alert("Escribe los números que vamos a sumar.")
+    number1 = prompt("Por favor, escribe el primer número. Puedes usar decimales.");
+    number1 = parseFloat(number1);
+    number2 = prompt("Escribe el segundo número.");
+    number2 = parseFloat(number2);
+    alert("Escribe el número con el cual se multiplicará la anterior suma.")
+    number3 = prompt("Escribe el tercer número.");
+    number3 = parseFloat(number3);
+    result = (number1 + number2) * number3;
+    result = result.toFixed(1);
+    alert("El resultado de sumar " + number1 + " con " + number2 + " y luego multiplicarlo por " + number3 + " es: " + result + ".");
+}
+
 
 
